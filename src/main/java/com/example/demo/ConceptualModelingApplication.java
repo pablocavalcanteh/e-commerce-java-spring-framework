128ee77c78ee7b6d72ae73476b280b1de945b2d0
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2020-02-12 13:48:51 -0300

    Many-to-many product and association with category

--- src/main/java/com/example/demo/ConceptualModelingApplication.java
+++ src/main/java/com/example/demo/ConceptualModelingApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Category;
+import com.example.demo.domain.Product;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.repositories.ProductRepository;
 
 @SpringBootApplication
 public class ConceptualModelingApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualModelingApplication.class, args);
@@ -24,12 +28,25 @@ public class ConceptualModelingApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		Category cat1 = new Category(null,"Computing");
-		
 		Category cat2 = new Category(null, "Office");
+		
+		Product p1 = new Product(null, "Computing", 2000.00);
+		Product p2 = new Product(null, "Printer", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
-
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
 		
 	}
 

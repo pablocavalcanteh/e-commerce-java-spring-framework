d33e997b3b1f92d897d1785616df5c559dcc889d
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2020-06-22 01:12:31 -0300

    Implementing smtp with the google server

--- src/main/java/com/example/demo/config/DevConfig.java
+++ src/main/java/com/example/demo/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.example.demo.service.DBService;
+import com.example.demo.service.EmailService;
+import com.example.demo.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -32,5 +34,12 @@ public class DevConfig {
 		return true;
 		
 	}
+	
+	@Bean
+	public EmailService emailService() {
+		
+		return new SmtpEmailService();
+	}
+	
 
 }
--- src/main/java/com/example/demo/service/DBService.java
+++ src/main/java/com/example/demo/service/DBService.java
@@ -110,7 +110,7 @@ public class DBService {
 		stateRepository.saveAll(Arrays.asList(state1, state2));
 		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
 
-		Client cli1 = new Client(null, "Maria Silva", "maria@gmail.com", "36378912377", ClientType.PHYSICALPERSON);
+		Client cli1 = new Client(null, "Maria Silva", "devphcavalcante@gmail.com", "36378912377", ClientType.PHYSICALPERSON);
 
 		cli1.getPhones().addAll(Arrays.asList("27363323", "9383839"));
 

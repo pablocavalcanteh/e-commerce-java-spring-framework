f9510f33bec49165864d4fcd9ddb8b3357b9b40b
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2020-06-28 15:13:34 -0300

    File in S3

--- src/main/java/com/example/demo/ConceptualModelingApplication.java
+++ src/main/java/com/example/demo/ConceptualModelingApplication.java
@@ -1,17 +1,11 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.service.S3Service;
-
 @SpringBootApplication
 public class ConceptualModelingApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualModelingApplication.class, args);
@@ -20,9 +14,6 @@ public class ConceptualModelingApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		s3Service.uploadFile("C:\\temp\\fotos\\aurora.jpg");
-		
-		
 	}
 
 }
--- src/main/java/com/example/demo/service/ClientService.java
+++ src/main/java/com/example/demo/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.domain.Address;
 import com.example.demo.domain.City;
@@ -35,6 +37,8 @@ public class ClientService {
 	private ClientRepository repo;
 	@Autowired
 	private AddressRepository addressRepository;
+	@Autowired
+	private S3Service s3Service;
 	
 	@Transactional
 	public Client insert(Client obj) {
@@ -110,5 +114,10 @@ public class ClientService {
 	private void updateData(Client newObj, Client obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		
+		return s3Service.uploadFile(multipartFile);
 	}
 }

dee2406f8d07d114ca788e0c4780985c56c30d12
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2020-06-28 00:26:59 -0300

    Saving first file to S3.

--- src/main/java/com/example/demo/ConceptualModelingApplication.java
+++ src/main/java/com/example/demo/ConceptualModelingApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.S3Service;
+
 @SpringBootApplication
 public class ConceptualModelingApplication implements CommandLineRunner {
 
-	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConceptualModelingApplication.class, args);
@@ -16,7 +20,7 @@ public class ConceptualModelingApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		
+		s3Service.uploadFile("C:\\temp\\fotos\\aurora.jpg");
 		
 		
 	}
--- src/main/java/com/example/demo/resources/AuthResource.java
+++ src/main/java/com/example/demo/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.example.demo.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.DTO.EmailDTO;
 import com.example.demo.security.JWTUtil;
 import com.example.demo.security.UserSpringSecurity;
+import com.example.demo.service.AuthService;
 import com.example.demo.service.UserService;
 
 @RestController
@@ -18,12 +22,21 @@ public class AuthResource {
 	
 	@Autowired
 	private JWTUtil	jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSpringSecurity user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 
